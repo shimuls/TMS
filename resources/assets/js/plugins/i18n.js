6b1e36442cd2524517f48401c5fb6901fb545f52
shimul.siddique@gmail.com <shimul.siddique@gmail.com>
2020-06-18 17:33:46 +0600

    Initial Commit

---
+++ resources/assets/js/plugins/i18n.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+import en from './en.json'
+import fr from './fr.json'
+import es from './es.json'
+import ar from './ar.json'
+import de from './de.json'
+import pt_BR from './pt-br.json'
+import it from './it.json'
+
+Vue.use(VueI18n)
+
+const i18n = new VueI18n({
+  locale: 'en',
+  messages: {
+    en,
+    fr,
+    es,
+    ar,
+    de,
+    pt_BR,
+    it,
+  },
+})
+
+export default i18n
